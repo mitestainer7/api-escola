f5fc53a3db61670b8a4dd3e2bf1de290dbe7f2ed
Nicolas Mitestainer <nicolas.mitestainer08@gmail.com>
2023-11-01 19:06:06 -0300

    criação de usuário

--- src/controllers/HomeController.js
+++ src/controllers/HomeController.js
@@ -3,12 +3,12 @@ import Aluno from '../models/Aluno';
 class HomeController {
   async index(req, res) {
     const novoAluno = await Aluno.create({
-      nome: 'Nicolas',
-      sobrenome: 'Mitestainer',
-      email: 'nicolas.mitestainer@email.com',
-      idade: 19,
-      peso: 82,
-      altura: 1.7
+      nome: 'Emelly',
+      sobrenome: 'Cunha',
+      email: 'emelly@email.com',
+      idade: 17,
+      peso: 50,
+      altura: 1.65
     })
     res.json(novoAluno);
   }
