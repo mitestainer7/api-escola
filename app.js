2e17936d5a893d04746bcd7b1f9f32256934cd86
Nicolas Mitestainer <nicolas.mitestainer08@gmail.com>
2023-11-03 14:50:16 -0300

    geração do JWT (JsonWebToken) do usuário

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 dotenv.config();
 
-import './src/database';
-import express from 'express';
-import homeRoutes from './src/routes/homeRoutes'
-import userRoutes from './src/routes/userRoutes'
+import "./src/database";
+import express from "express";
+import homeRoutes from "./src/routes/homeRoutes";
+import userRoutes from "./src/routes/userRoutes";
+import tokenRoutes from "./src/routes/tokenRoutes";
 
 class App {
   constructor() {
@@ -19,8 +20,9 @@ class App {
   }
 
   routes() {
-    this.app.use('/', homeRoutes)
-    this.app.use('/users/', userRoutes)
+    this.app.use("/", homeRoutes);
+    this.app.use("/users/", userRoutes);
+    this.app.use("/tokens/", tokenRoutes);
   }
 }
 
---
+++ src/controllers/TokenController.js
@@ -0,0 +1,41 @@
+import User from "../models/User";
+import jwt from "jsonwebtoken";
+
+class HomeController {
+  async store(req, res) {
+    const { email = "", password = "" } = req.body;
+
+    if(!email || !password) {
+      return res.status(401).json({
+        errors: ['Credenciais inválidas.']
+      })
+    }
+
+    const user = await User.findOne({ where: { email } });
+
+    if(!user) {
+      return res.status(401).json({
+        errors: ['Usuário não existente.']
+      })
+    }
+
+    if(!(await user.passwordIsValid(password))) {
+      return res.status(401).json({
+        errors: ['Senha inválida.']
+      })
+    }
+
+    const { id } = user;
+    const token = jwt.sign(
+      { id, email },
+      process.env.TOKEN_SECRET,
+      {
+      expiresIn: process.env.TOKEN_EXPIRATION,
+      }
+    )
+
+    return res.json({ token });
+  }
+}
+
+export default new HomeController();
