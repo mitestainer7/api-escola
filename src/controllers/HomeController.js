7a59282a20cac8e5a28257dd9aa43475c7957c7d
Nicolas Mitestainer <nicolas.mitestainer08@gmail.com>
2023-11-08 18:44:02 -0300

    CRUD de alunos

--- src/controllers/HomeController.js
+++ src/controllers/HomeController.js
@@ -1,16 +1,6 @@
-import Aluno from '../models/Aluno';
-
 class HomeController {
   async index(req, res) {
-    const novoAluno = await Aluno.create({
-      nome: 'Emelly',
-      sobrenome: 'Cunha',
-      email: 'emelly@email.com',
-      idade: 17,
-      peso: 50,
-      altura: 1.65
-    })
-    res.json(novoAluno);
+    res.json('Index');
   }
 }
 
